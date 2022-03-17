e417b0213fe3ac1b15ba3df134f2975d2c27a831
Ian Littman <iansltx@gmail.com>
2022-03-17 16:18:26 -0500

    Rename username/password to client ID + secret, fix inaccurate "application" reference to integration

--- examples/demo.php
+++ examples/demo.php
@@ -2,8 +2,8 @@
 
 // These values must be changed to what is used for HTTP Basic auth
 // Copy this file into another file that is untracked by git so client ID and secret aren't exposed.
-const USERNAME = '';
-const PASSWORD = '';
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -27,8 +27,8 @@ $sdkClient = LocalClient::createFromCredentials(USERNAME, PASSWORD);
  */
 
 // **WARNING**: this line will create a new Integration each time.
-// If this behavior is undesired, this link should be ran once, then commented out.
-$integration = $sdkClient->integrations()->create('Name of Application');
+// If this behavior is undesired, this link should be run once, then commented out.
+$integration = $sdkClient->integrations()->create('Name of Integration');
 
 // Extract Integration Key from $integration, it will be used to acquire a Policy entity
 echo $integration->getIntegrationKey() . PHP_EOL;
